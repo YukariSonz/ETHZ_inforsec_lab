61fad19f138eef3db76a270d50808c12ff462162
Yilei Liang <sonzbreaker@hotmail.com>
2020-12-18 20:03:30 +0800

    Finish better t1

--- Week_14/t1/module_7_password_cracker_t1.py
+++ Week_14/t1/module_7_password_cracker_t1.py
@@ -1,7 +1,7 @@
 # Some python code
 import sys
 import os 
-
+from string import ascii_lowercase
 
 folder = sys.argv[1]
 
@@ -13,67 +13,78 @@ folder = sys.argv[1]
 # E:0x40120f:C:11e:jnz 0x401217 
 # E:0x401217:C:180:mov eax, dword ptr [rbp-0x14] 
 
+# Repeat: E:0x40127e:C:2c6:sub dword ptr [rbp-0x10], 0x1
+
 
 Critical_Point = "E:0x40120f:C:11e:jnz 0x401217"
 CORRECT_SEQUENCE = "E:0x401211:C:7:add dword ptr [rbp-0xc], 0x1"
 WRONG_SEQUENCE = "E:0x401217:C:180:mov eax, dword ptr [rbp-0x14]"
+REPEAT = 'E:0x40127e:C:2c6:sub dword ptr [rbp-0x10], 0x1'
 LAST_LINE = 'E:0x441196:C:2cc:syscall'
+END_REPEAT_LINE = 'E:0x401288:C:7:add dword ptr [rbp-0x14], 0x1'
 
 CHECKED_LINE = 'E:0x4012a8:C:180:mov eax, 0x1'
+FALSE_LINE = 'E:0x4012af:C:180:mov eax, 0x0'
 
 
 traces = os.listdir(folder)
+CHART_LIST = [char for char in ascii_lowercase]
+
 
 def crack_password():
-    password_list = [''] * 15
-    max_length = 0 
+    
+    max_length = 0
+    max_file = ''
     for trace in traces:
-        word_length = len(trace) - 4
-        if word_length > max_length:
-            max_length = word_length
-        local_counter = 0
-        wrong_counter = 0
-        trace_file = open(folder + '/' +trace)
-        line = trace_file.readline()
-        while not (LAST_LINE in line):
-            if Critical_Point in line:
+        if len(trace) > max_length:
+            max_length = len(trace)
+            max_file = trace
+    password_list = [''] * max_length
+    
+    trace_file = open(folder + '/' + max_file)
+    line = trace_file.readline()
+    local_counter = 0
+    while not (LAST_LINE) in line:
+        if Critical_Point in line:
+            line = trace_file.readline()
+            if CORRECT_SEQUENCE in line:
+                password_list[local_counter] = max_file[local_counter]
+                local_counter += 1
+            if WRONG_SEQUENCE in line:
                 line = trace_file.readline()
-                if CORRECT_SEQUENCE in line:
-                    password_list[local_counter] = trace[local_counter]
-                if WRONG_SEQUENCE in line:
+                counter = 0
+                while not END_REPEAT_LINE in line:
+                    if REPEAT in line:
+                        counter += 1
+                        print(counter)
                     line = trace_file.readline()
-                    wrong_counter += 1
+                character = max_file[local_counter]
+                #Calculate the new character
+                character_int = CHART_LIST.index(character)
+                new_index = (counter + character_int)%26
+                new_character = CHART_LIST[new_index]
+                password_list[local_counter] = new_character
                 local_counter += 1
 
-            elif CHECKED_LINE in line:
-                password = ""
-                for char in password_list:
-                    if char != '':
-                        password += char
+        if CHECKED_LINE in line:
+            password = ""
+            for char in password_list:
+                if char != '':
+                    password += char
+            password += " complete"
+            return password
+        elif FALSE_LINE in line:
+            password = ""
+            for char in password_list:
+                if char != '':
+                    password += char
+            if max_length >= local_counter:
                 password += " complete"
-                return password
             else:
-                line = trace_file.readline()
-        # Special Case, the correct password is part of the trail
-        if local_counter < word_length:
-            if wrong_counter == 0:
-                password = ""
-                for char in password_list:
-                    if char != '':
-                        password += char
-                password += " complete"
-                return password
-
-
-    password = ""
-    password_list = password_list[:max_length]
-    for char in password_list:
-        if char != '':
-            password += char
+                password += " partial"
+            return password
         else:
-            password += "_"
-    password += " partial"
-    return password
+            line = trace_file.readline()
 
     
 
